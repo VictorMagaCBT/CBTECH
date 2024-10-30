78d54e965d5cf26e4b9165d5f14439be873e5775
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2024-10-30 18:47:46 +0000

    removed React from clientes.tsx

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Users, Mail, Phone, Building, CreditCard, ChevronLeft, ChevronRight } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { apiService } from '../api';
